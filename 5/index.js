79d860af0f42b7f3521b40bb727a841fa9d4b2b6
Ovbml <89481220+ovbml@users.noreply.github.com>
2022-09-10 20:15:32 +0300

    Add files via upload

---
+++ 5/index.js
@@ -0,0 +1,69 @@
+const words = document.querySelectorAll('.main__word');
+const wordsCount = document.querySelector('.main__words-count');
+
+let blockFilter = false;
+
+function filter(entry = 'all') {
+    const regexp = new RegExp(entry.replaceAll('ё', 'е')
+                                   .replaceAll('е', '[её]'),
+                             'gi');
+
+    for ( let word of words ) {
+        word.innerText = word.dataset.initValue;
+
+        if ( regexp.test(word.innerText) ) {
+            // underline matches
+            const wordMatch = word.innerText.match(regexp)[0];
+            word.innerHTML = word.innerText.replaceAll(
+                regexp,
+                '<span class="main__word-entry">' + 
+                wordMatch + 
+                '</span>'
+            );
+
+            // animated show
+            word.style.display = 'block';
+            setTimeout(() => {word.classList.add('main__word_active')}, 0);
+        } else {
+            // animated hide
+            word.classList.remove('main__word_active');
+            setTimeout(() => {
+                word.style.display = 'none';
+                word.innerHTML = word.innerText;
+            }, 300);
+        }
+    }
+
+    setTimeout(pretty, 0);
+}
+
+function pretty() {
+    const filteredWords = document.querySelectorAll('.main__word_active');  
+    wordsCount.innerText = filteredWords.length;
+
+    if ( filteredWords.length === 0 )
+        return;
+
+    // uppercase the first char of the first element
+    const first = filteredWords[0];
+    first.innerHTML = first.innerHTML.replace(
+        first.dataset.initValue[0],
+        first.dataset.initValue[0].toUpperCase()
+    );
+
+    for ( let i = 0; i < filteredWords.length; i++ )
+        filteredWords[i].innerHTML += ( i === filteredWords.length - 1 ) ? '.' : ',';
+}
+
+document.querySelector('.main__input').oninput = function() {
+    if ( !blockFilter ) {
+        blockFilter = true;
+
+        setTimeout(input => {
+            blockFilter = false;
+            filter(input.value);
+        }, 400, this);
+    }
+};
+
+pretty();
